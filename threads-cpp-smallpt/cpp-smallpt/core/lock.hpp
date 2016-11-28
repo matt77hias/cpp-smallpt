7443b0b116d482816b3d97997fbd1f44cb31310b
Matthias Moulin <matthias.moulin@gmail.com>
2016-11-28 16:30:22 +0100

    Update lock.hpp

--- threads-cpp-smallpt/cpp-smallpt/core/lock.hpp
+++ threads-cpp-smallpt/cpp-smallpt/core/lock.hpp
@@ -17,12 +17,10 @@ public:
 private:
 
 	Mutex() {
-		// Initialize a critical section object.
 		InitializeCriticalSection(&m_critical_section);
 	}
 	Mutex(Mutex &mutex);
 	~Mutex() {
-		// Release all resources used by an unowned critical section object. 
 		DeleteCriticalSection(&m_critical_section);
 	}
 
@@ -32,12 +30,9 @@ private:
 struct MutexLock final {
 
 	MutexLock(Mutex &mutex) : m_mutex(mutex) {
-		// Wait for ownership of the specified critical section object. 
-		// The function returns when the calling thread is granted ownership.
 		EnterCriticalSection(&m_mutex.m_critical_section);
 	}
 	~MutexLock() {
-		// Release ownership of the specified critical section object.
 		LeaveCriticalSection(&m_mutex.m_critical_section);
 	}
 
@@ -54,7 +49,6 @@ public:
 
 	Semaphore();
 	~Semaphore() {
-		// Closes an open object handle.
 		CloseHandle(m_handle);
 	}
 
@@ -72,41 +66,24 @@ class ConditionVariable final {
 public:
 
 	ConditionVariable() : m_nb_waiters(0) {
-		// Initialize the critical section objects
-		// for the number of waiters and condition.
 		InitializeCriticalSection(&m_nb_waiters_mutex);
 		InitializeCriticalSection(&m_condition_mutex);
 
-		// Creates or opens a named or unnamed event object.
-		// On success, a handle to the event object is returned.
-		m_events[SIGNAL] = CreateEvent(
-								nullptr,  // no security
-								FALSE, // auto-reset event object
-								FALSE, // non-signaled initial state
-								nullptr); // unnamed event object
-		m_events[BROADCAST] = CreateEvent(
-								nullptr,  // no security
-								TRUE,  // manual-reset event object
-								FALSE, // non-signaled initial state
-								nullptr); // unnamed event object
+		m_events[SIGNAL] = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+		m_events[BROADCAST] = CreateEvent(nullptr, TRUE, FALSE, nullptr);
 	}
-	~ConditionVariable() {
-		// Release all resources used by an unowned critical section object. 
+	~ConditionVariable() { 
 		DeleteCriticalSection(&m_nb_waiters_mutex);
 		DeleteCriticalSection(&m_condition_mutex);
-
-		// Close the open event handles.
+		
 		CloseHandle(m_events[SIGNAL]);
 		CloseHandle(m_events[BROADCAST]);
 	}
 
 	void Lock() {
-		// Wait for ownership of the specified critical section object. 
-		// The function returns when the calling thread is granted ownership.
 		EnterCriticalSection(&m_condition_mutex);
 	}
 	void Unlock() {
-		// Release ownership of the specified critical section object.
 		LeaveCriticalSection(&m_condition_mutex);
 	}
 
